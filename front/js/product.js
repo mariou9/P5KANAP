5205d87442d71be0c4ea519cc90805f17c28ff9d
Marielle Jeantheodore <mariou9@hotmail.com>
2022-02-25 15:16:18 +0100

    ajout d'elements

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,3 @@
-/*Lien entre le produit et la page*/
-
 var str = window.location.href;
 var url = new URL(str);
 var idProduct = url.searchParams.get("id");
@@ -11,7 +9,7 @@ const quantityPicked = document.querySelector("#quantity");
 
 getArticle();
 
-/* Récupération des articles de l'API*/
+// Récupération des articles de l'API
 function getArticle() {
     fetch("http://localhost:3000/api/products/" + idProduct)
     .then((res) => {
@@ -29,34 +27,4 @@ function getArticle() {
     .catch((error) => {
         console.log("Erreur de la requête API");
     })
-}
-    
-function getPost(article){
-    // Insertion de l'image
-    let productImg = document.createElement("img");
-    document.querySelector(".item__img").appendChild(productImg);
-    productImg.src = article.imageUrl;
-    productImg.alt = article.altTxt;
-
-    // Modification du titre "h1"
-    let productName = document.getElementById('title');
-    productName.innerHTML = article.name;
-
-    // Modification du prix
-    let productPrice = document.getElementById('price');
-    productPrice.innerHTML = article.price;
-
-    // Modification de la description
-    let productDescription = document.getElementById('description');
-    productDescription.innerHTML = article.description;
-
-    // Insertion des options de couleurs
-    for (let colors of article.colors){
-        console.table(colors);
-        let productColors = document.createElement("option");
-        document.querySelector("#colors").appendChild(productColors);
-        productColors.value = colors;
-        productColors.innerHTML = colors;
-    }
-    addToCart(article);
 }
