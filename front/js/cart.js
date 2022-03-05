46a14b53daa233bfa7886c1cc95b7bc3802420fe
Marielle Jeantheodore <mariou9@hotmail.com>
2022-03-05 10:45:43 +0100

    Inserer un produit et ses détails dans la page produit

---
+++ front/js/cart.js
@@ -0,0 +1,4 @@
+//Initialisation du local storage
+let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
+console.table(produitLocalStorage);
+const positionEmptyCart = document.querySelector("#cart__items");
